eed5facfd1d3bcde2fc8cfa02b56d39ad54e4391
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-12-24 17:24:02 +0530

    After fixing deployment security

--- api/index.js
+++ api/index.js
@@ -28,8 +28,7 @@ dotenv.config();
 const corsOptions = {
     origin: [
         "https://orm-frontend-eight.vercel.app",
-        "https://oms-customer-three.vercel.app/",
-        "https://oms-customer-two.vercel.app",
+        "https://oms-customer-three.vercel.app",
         "http://localhost:3000",
         "http://localhost:3001"
     ],
