0fec0b20d49fd1683ad28a66ef8aab11b5865e2e
Bhishek-Parmar <itsbhishek@gmail.com>
2024-12-29 10:59:32 +0530

    update table routes

--- routes/tableRouter.js
+++ routes/tableRouter.js
@@ -5,11 +5,7 @@ import { getTableById, getTables, createTable, updateTable, deleteTable, getOrde
 
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
-router.get('/:id', protect, getTableById);
 
->>>>>>> 8274f71185ae61b7c6f50b040da2f96ec9284500
 router.get('/', protect, attachHotelId, getTables);
 router.post('/', protect, createTable);
 router.get('/bill/:tableId', protect, validateOwnership, generateTableBill);
@@ -17,8 +13,8 @@ router.get('/table/:tableId', protect, validateOwnership, getOrdersByTable);
 router.get('/get-customer/:tableId', protect, validateOwnership, getCustomerDetails)
 
 
+router.get('/:id', protect, getTableById);
 router.get('/:id', protect, validateOwnership, getTableById);
-
 router.put('/:tableId', protect, updateTable);
 
 router.delete('/:id', protect, deleteTable);
